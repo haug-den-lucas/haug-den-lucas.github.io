18d4519ab5c939d8adc748be1923472ae0851573
Lucas <lucas.haug@ipvs.uni-stuttgart.de>
2026-03-11 17:43:54 +0100

    Add new publications to gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -134,18 +134,36 @@ const CONFIG = {
   ],
   publications: [
     {
+      title: 'Compensating the Packet Delay Variation for 6G Integrated with IEEE Time-Sensitive Networking',
+      conferenceName: '33rd International Conference on Real-Time Networks and Systems',
+      journalName: '',
+      authors: 'Marilet De Andrade; Joachim Sachs; Lucas Haug; Simon Egger; Frank Dürr; Balázs Varga; Janos Farkas; György Miklós',
+      link: 'https://doi.org/10.48550/arXiv.2602.21444',
+      description:
+        '6G is deemed as a key technology to support emerging applications with stringent requirements for highly dependable and timecritical communication. In this paper, we investigate 6G networks integrated with TSN and how to compensate for wireless stochastic behavior which involves a large intrinsic packet delay variation. We evaluate a 6G solution to reduce packet delay variation that is based on de-jittering. For this, we propose to use virtual timeslots for providing the required time-awareness. We discuss the benefits of the proposed solution while evaluating the impact of the timeslot size on the number of schedulable TSN streams.'
+    },
+    {
+      title: 'Analysis of Time Synchronization for 6G-TSN Networks with Hot Standby',
+      conferenceName: '34th International Conference on Computer Communications and Networks (ICCCN)',
+      journalName: '',
+      authors: 'Mahin Ahmed; Lucas Haug; Raheeb Muzaffar; Damir Hamidovic; Armin Hadziaganovic; Hans-Peter Bernhard; Marilet De Andrade; János Farkas',
+      link: 'https://doi.org/10.1109/ICCCN65249.2025.11134023',
+      description:
+        'Reliable time synchronization is of utmost importance within 6G networks, particularly when envisioning their role in future industrial automation scenarios that demand deterministic communication. In particular, we focus on the integration of 6G with time-sensitive networking (TSN). Traditionally, the best timeTransmitter clock algorithm (BTCA) is used to dynamically select a grandmaster (GM) for TSN nodes in a 6G-TSN network. However, BTCA’s performance in case of link or device failures is categorized to be slow. The IEEE 802.1ASdm standard addresses this limitation by introducing a hot standby GM in the network. In this paper, we extend the hot standby amendment to 6G-TSN networks to support continuous time synchronization. For the analysis, we develop a simulation framework in OMNeT++. We analyze the performance in terms of clock drift and out-of-sync time for different time synchronization scenarios in case of failures. Having two synchronized GMs in the network reduces the out-of-sync time in case of failure when compared to BTCA.'
+    },
+    {
       title: 'Simulating and Emulating the Characteristic Packet Delay of Logical 5G TSN Bridges',
-      conferenceName: '',
-      journalName: '4. KuVS Fachgespräch "Network Softwarization"',
+      conferenceName: '4. KuVS Fachgespräch "Network Softwarization"',
+      journalName: '',
       authors: 'Lucas Haug; Frank Dürr; Simon Egger; Lorenz Grohmann; James Gross; Gourav Prateek Sharma; Joachim Sachs',
-      link: 'http://dx.doi.org/10.15496/publikation-105097  ',
+      link: 'http://dx.doi.org/10.15496/publikation-105097',
       description:
         'Due to the demand of many time-sensitive mobile applications, there is currently a strong trend to extend Time- Sensitive Networking (TSN) to the wireless domain. In particular, 3GPP has speficied a logial 5G TSN bridge, which implements the same functions as wireline TSN bridges (e.g. the Time- Aware Shaper of IEEE 802.1Qbv). However, a logical bridge has fundamentally different port-to-port delay properties than its wired counterpart. In this paper, we present two open-source tools and open latency data sets for simulating and emulating the characteristic port-to-port delay of logical 5G TSN bridges.'
     },
     {
       title: 'How to Optimize Joint Routing and Scheduling Models for TSN Using Integer Linear Programming',
-      conferenceName: '',
-      journalName: '29th International Conference on Real-Time Networks and Systems',
+      conferenceName: '29th International Conference on Real-Time Networks and Systems',
+      journalName: '',
       authors: 'David Hellmanns; Lucas Haug; Moritz Hildebrand; Frank Dürr; Stephan Kehrer; René Hummen',
       link: 'https://dl.acm.org/doi/10.1145/3453417.3453421',
       description:
